04e3db8670046cb385859b9cfa1c652761bf48b8
Tringapps-Rishikumar <rishikumar.d@tringapps.com>
2023-03-20 10:47:46 +0530

    Initial commit

---
+++ src/main/java/point/Point.java
@@ -0,0 +1,20 @@
+package point;
+
+public class Point implements Cloneable{
+    public String x;
+    public String y;
+    public Point(String x, String y)
+    {
+        this.x=x;
+        this.y=y;
+    }
+
+    public Object clone() throws CloneNotSupportedException
+    {
+        return  super.clone();
+    }
+
+    public boolean check(String x1,String y1) {
+        return x.equals(x1) &&y.equals(y1);
+    }
+}
