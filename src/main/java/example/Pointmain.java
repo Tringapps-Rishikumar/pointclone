ec61e6ece062115a660afe16361e59e0ede28198
Tringapps-Rishikumar <rishikumar.d@tringapps.com>
2023-03-20 11:22:52 +0530

    done

--- src/main/java/example/Pointmain.java
+++ src/main/java/example/Pointmain.java
@@ -1,7 +1,27 @@
 package example;
-import calculation.Calculation;
+import point.Pointlogic;
+import java.util.Scanner;
+import java.util.logging.Logger;
+
 public class Pointmain{
     public static void main(String []args)throws CloneNotSupportedException {
-        new Calculation();
+
+        Logger log =  Logger.getLogger("points");
+        Scanner sc = new Scanner(System.in);
+        log.info("Enter Value for x:");
+        Float x = sc.nextFloat();
+        log.info("Enter Value for y:");
+        Float y = sc.nextFloat();
+        sc.close();
+
+        Pointlogic obj= new Pointlogic(x,y);
+        Pointlogic obj2 =obj.point();
+
+        if(obj2.testEqual()) {
+            log.info("Values are same");
+        }
+        else {
+            log.info("Values are not same");
+        }
     }
 }
---
+++ src/main/java/point/Pointlogic.java
@@ -0,0 +1,29 @@
+package point;
+import java.util.logging.Logger;
+
+public class Pointlogic implements Cloneable{
+
+    Float x;
+    Float y;
+    Logger log =  Logger.getLogger("Point");
+    public Pointlogic(Float x, Float y){
+        this.x=x;
+        this.y=y;
+    }
+    public boolean testEqual() {
+        return x.equals(y);
+    }
+
+    public Pointlogic point() {
+        try {
+            return (Pointlogic) super.clone();
+
+        }catch(CloneNotSupportedException e) {
+            log.info("error"+e);
+            return this;
+
+        }
+
+    }
+
+}
