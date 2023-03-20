4a4ebbe4fe9d30ba82e3af2e6f848c4f2cda0a58
Tringapps-Rishikumar <rishikumar.d@tringapps.com>
2023-03-20 12:22:19 +0530

    done

--- src/main/java/point/Pointlogic.java
+++ src/main/java/point/Pointlogic.java
@@ -19,15 +19,7 @@ public class Pointlogic implements Cloneable{
     }
 
 
-//    public Pointlogic point() {
-//        try {
-//            return (Pointlogic) super.clone();
-//
-//        }catch(CloneNotSupportedException e) {
-//            log.info("error"+e);
-//            return this;
-//
-//        }
+
 
     }
 
